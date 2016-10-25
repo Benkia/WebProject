d24ea59c4b2f886416279a2f747d7a96cc6f9a01
Aviad Avretz <aviad31194@gmail.com>
2016-10-25 22:50:30 +0300

    various fixes

---
+++ WebProject/Controllers/AbstractController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebProject.Controllers
+{
+    public abstract class AbstractController : Controller
+    {
+        protected override void OnActionExecuting(ActionExecutingContext ctx)
+        {
+            if (Request.IsAuthenticated)
+            {
+                base.OnActionExecuting(ctx);
+            }
+            else
+            {
+                ctx.Result = base.RedirectToAction("Login", "Account");
+            }
+        }
+    }
+}
--- WebProject/Controllers/HomeController.cs
+++ WebProject/Controllers/HomeController.cs
@@ -1,110 +1,4 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Threading.Tasks;
-//using System.Web.Mvc;
-//using WebProject.Models;
-//using System.Data.Entity;
-
-//namespace WebProject.Controllers
-//{
-//    public class HomeController : Controller
-//    {
-//        private ApplicationDbContext db = new ApplicationDbContext();
-
-//        // GET: Posts
-//        public ActionResult Index()
-//        {
-//            return View(db.Posts.Include(p => p.Fan).ToList());
-//        }
-
-//        public ActionResult SinglePost(int id)
-//        { 
-//            return View(db.Posts.Include(p => p.Fan).Single(post => post.ID.Equals(id)));
-//        }
-
-//        public ActionResult About()
-//        {
-//            ViewData["Message"] = "This is a web project by Ben Kiani and Naveh Ohana.";
-//            return View();
-//        }
-
-//        public ActionResult Contact()
-//        {
-//            ViewData["Message"] = "Contact us!";
-//            return View();
-//        }
-//        public ActionResult Graphs()
-//        {
-//            return View();
-//        }
-//        public ActionResult DataGraph1()
-//        {
-//            var result = from fan in db.fans
-//                         join post in db.Posts on fan.ID equals post.FanID into FansPosts
-//                         from fanPost in FansPosts
-//                         group fanPost by fan.PazamInClub into FanGroupPost
-//                         select new { PazamInClub = FanGroupPost.Key, Number_Of_Posts = FanGroupPost.Count() };
-
-//            return Json(result, "application/json", JsonRequestBehavior.AllowGet);
-//        }
-//        public ActionResult DataGraph2()
-//        {
-//            IEnumerable<dynamic> result = from fan in db.fans
-//                                          join post in db.Posts on fan.ID equals post.FanID
-//                                          select new
-//                                          {
-//                                              name = fan.FirstName + " " + fan.LastName,
-//                                              postId = post.ID,
-//                                              children = post.Comments.Select(comment => new { name = comment.Fan.FirstName + " " + comment.Fan.LastName })
-//                                          };
-
-//            // Key = postId
-//            Dictionary<int, List<Object>> allPosts = new Dictionary<int, List<object>>();
-
-//            foreach (var currentPost in result)
-//            {
-//                if (!allPosts.ContainsKey(currentPost.postId))
-//                {
-//                    allPosts.Add(currentPost.postId, new List<Object>());
-//                }
-
-//                allPosts[currentPost.postId].Add(currentPost.children);
-//            }
-
-//            List<dynamic> finalList = new List<dynamic>();
-
-//            foreach (var currentPost in allPosts)
-//            {
-//                int postID = currentPost.Key;
-
-//                Post post = db.Posts.First(p => p.ID.Equals(postID));
-//                Fan fan = db.fans.First(f => f.ID.Equals(post.FanID));
-
-//                finalList.Add(new { name = fan.FirstName +
-//                                           " " +
-//                                           fan.LastName,
-//                                    children = currentPost.Value });
-//            }
-
-//            //var result = from post in db.Posts
-//            //             select new
-//            //             {
-//            //                 name = post.Title,
-//            //                 children = post.Comments.Select(comment => new { name = comment.Fan.FirstName + " " + comment.Fan.LastName })
-//            //             };
-
-//            return Json(new { name = "Root", children = result }, "application/json", JsonRequestBehavior.AllowGet);
-//        }
-
-//        public ActionResult Error()
-//        {
-//            return View();
-//        }
-//    }
-//}
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -114,7 +8,7 @@ using System.Data.Entity;
 
 namespace WebProject.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : AbstractController
     {
         private ApplicationDbContext db = new ApplicationDbContext();
 
@@ -204,22 +98,6 @@ namespace WebProject.Controllers
             //             };
 
             return Json(new { name = "Root", children = result }, "application/json", JsonRequestBehavior.AllowGet);
-        }
-
-        public ActionResult DataMap()
-        {
-            List<object> allFans = new List<object>();
-            foreach (var item in db.fans.Where(fan => fan.Address != null))
-            {
-                allFans.Add(new { name = item.ID, Address = item.Address });
-            }
-
-            return Json(allFans, "application/json", JsonRequestBehavior.AllowGet);
-        }
-
-        public ActionResult Maps()
-        {
-            return View();
         }
 
         public ActionResult Error()
